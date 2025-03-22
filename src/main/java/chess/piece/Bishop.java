d45be9faf99d0cd6d4915cd6705a2a3aeec1a5a0
(minjae8563) <(minjae8563@naver.com)>
2025-03-22 17:24:32 +0900

    feat: 기능 구현 완료

--- src/main/java/chess/piece/Bishop.java
+++ src/main/java/chess/piece/Bishop.java
@@ -1,5 +1,20 @@
 package chess.piece;
 
-public class Bishop {
+import chess.Color;
+import chess.Location;
+import java.util.List;
 
+public class Bishop extends Piece {
+
+    public Bishop(Location location, String name, PieceType pieceType) {
+        super(location, name, pieceType);
+    }
+
+    @Override
+    public Boolean isCanMoveDestination(Location destination) {
+        int dx = getLocation().getX() - destination.getX();
+        int dy = getLocation().getY() - destination.getY();
+
+        return Math.abs(dx) == Math.abs(dy);
+    }
 }
